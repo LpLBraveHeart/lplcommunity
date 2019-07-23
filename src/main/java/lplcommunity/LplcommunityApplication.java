7fefc8cf136768e4b47a3a9c36c3d3c638e3cc9e
LpLBraveHeart <1987721909@qq.com>
2019-07-23 19:44:08 +0800

    init repo

---
+++ src/main/java/lplcommunity/LplcommunityApplication.java
@@ -0,0 +1,10 @@
+package lplcommunity;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LplcommunityApplication {
+    public static void main(String[] args) {
+
+    }
+}
